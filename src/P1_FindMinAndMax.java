51c167444e4b4a8163e520b5c011b71705aa934e
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-16 20:04:18 -0500

    Update P1_FindMinAndMax.java

--- src/P1_FindMinAndMax.java
+++ src/P1_FindMinAndMax.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Clayton Bruce
+	Date: 4/16/2018
 */
 
 import java.util.Scanner;
@@ -9,18 +11,33 @@ import java.util.Scanner;
 public class P1_FindMinAndMax {
 
 	public static void main(String[] args) {
-        Scanner console = new Scanner( System.in );
-        
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        
-        // YOUR CODE STARTS HERE
+		Scanner console = new Scanner(System.in);
 
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 
-        // YOUR CODE ENDS HERE
-        
-        System.out.println( "Maximum was " + max );
-        System.out.println( "Minimum was " + min );
-    }
+		// YOUR CODE STARTS HERE
+
+		int i = 0;
+
+		while (i >= 0) {
+			System.out.print("Enter a number (or -1 or below to stop) : ");
+			i = console.nextInt();
+
+			if (i > max) {
+				max = i;
+			}
+			else if (i < min) {
+				min = i;
+			}
+
+		}
+		System.out.println("Entered a negitive So we stopped");
+
+		// YOUR CODE ENDS HERE
+
+		System.out.println("Maximum was " + max);
+		System.out.println("Minimum was " + min);
+	}
 
 }

29a972e8798e84162cbf707e8320e113fbc9b5a1
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-16 21:10:03 -0500

    Update P2_InfiniteGuessingGame.java

--- src/P2_InfiniteGuessingGame.java
+++ src/P2_InfiniteGuessingGame.java
@@ -1,7 +1,7 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Clayton Bruce
+	Date: 4/16/2018
 */
 
 
@@ -11,24 +11,44 @@ import java.util.Scanner;
 public class P2_InfiniteGuessingGame {
 
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		String playagain ="";
+		//making the whole game replayable
+		//THE DO WHILE LOOP WONT EXECUTE A SEOCND TIME
+		do {
 		int secretNumber = getSecretNumber();
 		int numberOfGuessesAllowed = 7;
 
 		outputWelcomeMessage();
 		int guessesTaken = playGame(secretNumber, numberOfGuessesAllowed);
 		outputEndingMessage(guessesTaken, numberOfGuessesAllowed, secretNumber);
+
+		System.out.println("Type (quit) to stop playing");
+		
+		playagain = console.next();
+
+		}while(!playagain.equals( "quit"));
+		
+		System.out.println("Thanks for playing goodbye");
 	}
 
 	public static void outputWelcomeMessage() {
 		System.out.println("This is a guessing game.");
-		System.out.println("I picked a random number from 1 to 100");
+		System.out.println("I picked a random number from 1 to 100.");
+		System.out.println("For now you get 7 guesses");
 	}
 
 	public static void outputEndingMessage(int guessesTaken, int numberOfGuessesAllowed, int secretNumber) {
+		int wins=0;
+		int losses = 0;
 		if (guessesTaken < numberOfGuessesAllowed) {
 			System.out.println("Congratuations, you guessed it!");
+			wins++;
+			System.out.println("You have won: "+wins+" times!");
 		} else {
 			System.out.println("The secret number was " + secretNumber + ". Try again.");
+			losses++;
+			System.out.println("You have lost "+ losses+" times!");
 		}
 	}
 
@@ -61,6 +81,12 @@ public class P2_InfiniteGuessingGame {
 				return numOfGuessesTaken;
 			} else {
 				System.out.println("Nope, guess again. ");
+				if(nextGuess < secretNumber) {
+					System.out.println("Your Guess is too low ");
+				}
+				if(nextGuess > secretNumber) {
+					System.out.println("Your Guess is too high");
+				}
 				numOfGuessesTaken++;
 			}
 		}
